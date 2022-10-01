a63c84053547b342c172f9da8c3dd3ca3081e353
syke99 <quinnmillican@gmail.com>
2022-10-01 11:03:17 -0500

    updated trier to use *argument.Arguments in trier.Trier.Try()

--- try/trier.go
+++ try/trier.go
@@ -2,19 +2,20 @@ package try
 
 import (
 	"errors"
+	"github.com/syke99/escargot/argument"
 	err "github.com/syke99/escargot/error"
 	"github.com/syke99/escargot/shell"
 )
 
-type tryFunc func(args ...any) *shell.Shell
+type tryFunc func(args ...argument.Arguments) *shell.Shell
 
-type catchFunc func(err *err.EscargotError, args ...any)
+type catchFunc func(err *err.EscargotError, args ...argument.Arguments)
 
 // Trier will handle trying the TryFunc provided and execute the provided CatchFunc
 // on error
 type Trier struct {
-	tryFunc   func(args ...any) *shell.Shell
-	catchFunc func(err *err.EscargotError, args ...any)
+	tryFunc   func(args ...argument.Arguments) *shell.Shell
+	catchFunc func(err *err.EscargotError, args ...argument.Arguments)
 }
 
 // NewTrier will return a new Trier with the provided TryFunc and CatchFunc
@@ -33,7 +34,7 @@ func NewTrier(try tryFunc, catch catchFunc) (Trier, error) {
 // Try tries the Trier's TryFunc with the provided tryArgs, and on error,
 // will execute the Trier's CatchFunc with the provided catchArgs. It will
 // return a *shell.Shell to access any values and/or errors
-func (t Trier) Try(tryArgs []any, catchArgs []any) *shell.Shell {
+func (t Trier) Try(tryArgs []argument.Arguments, catchArgs []argument.Arguments) *shell.Shell {
 	result := t.tryFunc(tryArgs...)
 
 	if result.GetErrStatus() {
